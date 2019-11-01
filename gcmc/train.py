cec61edbe3859365afd3452dd637e99f4def6272
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-02 09:04:03 +1100

    Update train.py

--- gcmc/train.py
+++ gcmc/train.py
@@ -401,10 +401,10 @@ wait = 0
 
 print('Training...')
 file_write = open('data.csv','a')
-    header=['epoch','train_rmse','val_loss','val_rmse','time']
-        csvreader = csv.reader(file_write)
-        writer = csv.writer(file_write, delimiter=',')
-        writer.writerow([h for h in header])  
+header=['epoch','train_rmse','val_loss','val_rmse','time']
+csvreader = csv.reader(file_write)
+writer = csv.writer(file_write, delimiter=',')
+writer.writerow([h for h in header])  
 
 for epoch in range(NB_EPOCH):
 

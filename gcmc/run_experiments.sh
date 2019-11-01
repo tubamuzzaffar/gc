94a42d3fec6b3a28764e5a59a5d2fffb8c372726
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-01 23:16:44 +1100

    Update run_experiments.sh

--- gcmc/run_experiments.sh
+++ gcmc/run_experiments.sh
@@ -3,6 +3,6 @@
 for i in 1000, 750, 500, 250, 125
 do
 # Movielens 100K on official split with features
-python train.py -d ml_100k --accum stack -do 0.7 -nleft -nb 2 -e 1000 --features --feat_hidden 10 --testing
+python train.py -d ml_100k --accum stack -do 0.7 -nleft -nb 2 -e i --features --feat_hidden 10 --testing
 done > ml_100k_feat_testing.txt  2>&1
 

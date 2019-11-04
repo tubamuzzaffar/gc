c465cb6df20b6f03b95ef4a0752d09f4642642bc
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-04 20:48:21 +1100

    Update grid.py

--- gcmc/grid.py
+++ gcmc/grid.py
@@ -1,3 +1,11 @@
 import subprocess
+from sklearn.grid_search import ParameterGrid
 
-subprocess.call("python train.py -d ml_100k --accum stack -do 0.7 -nleft -nb 2 -e 1000 --features --feat_hidden 10 --testing", shell=True)
+
+param_grid = {'epoch' :[1000], 'lr':[0.0005, 0.001,0.00146,0.01,0.015,0.0175,0.02], 'dr':[0.03,0.4,0.5,0.6,0.7,0.8], 
+              'hn':[[500,75],[100,75],[500,25],[100,25],[500,150],[500,300],[450,150]], 'hf':[5,10,15]}
+
+grid = ParameterGrid(param_grid)
+
+for params in grid:
+subprocess.call("python train.py -d ml_100k --accum stack -do" + str(params['dr']) + "-nleft -nb 2 -e" + str(params['epoch']) + "--features --feat_hidden"+ str(params['hf']) +" --testing", shell=True)

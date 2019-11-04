178e9faac57b1d3b98ddbc9220312bff4c947f6a
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-05 05:49:26 +1100

    Update grid.py

--- gcmc/grid.py
+++ gcmc/grid.py
@@ -12,10 +12,8 @@ for params in grid:
   dr = str(params['dr'])
   lr = str(params['lr'])
   hf = str(params['hf'])
-  x = [x for (x,y) in params['hn']]
-  y = [y for (x,y) in params['hn']]
-       
-  print(x,y)
+  x = params['hn']   
+  print(x)
   #subprocess.check_call(["./train.py", "-d", "ml_100k","--accum",  str(params['ac']),"-do",str(params['dr']),"-nleft", "-nb" , "2", "-e",str(params['epoch']),"--features", "--feat_hidden", str(params['hf']),"--testing" ])
   subprocess.call("python train.py -d ml_100k --accum " + ac +" -do " + dr + " -nleft -nb 2 -e " + epoch + " --features --feat_hidden "+ hf +" --testing --learning_rate " + lr ,shell=True)
 

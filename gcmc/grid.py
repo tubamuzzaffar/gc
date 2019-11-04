1928eb1a1d3d73b952404a7389d895b05ebe699e
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-05 00:30:36 +1100

    Update grid.py

--- gcmc/grid.py
+++ gcmc/grid.py
@@ -1,8 +1,7 @@
 import subprocess
 from sklearn.model_selection import ParameterGrid
 
-param_grid = {'epoch' :[10], 'lr':[0.0005, 0.001,0.00146,0.01,0.015,0.0175,0.02], 'dr':[0.03,0.4,0.5,0.6,0.7,0.8], 
-              'hn':[[500,75],[100,75],[500,25],[100,25],[500,150],[500,300],[450,150]], 'hf':[5,10,15], 'ac':['stack','sum']}
+param_grid = {'epoch' :[10], 'lr':[0.0005, 0.001,0.00146,0.01,0.015,0.0175,0.02], 'dr':[0.03,0.4,0.5,0.6,0.7,0.8],'hf':[5,10,15], 'ac':['stack','sum']}
 
 grid = list(ParameterGrid(param_grid))
 
@@ -12,8 +11,7 @@ for params in grid:
   ac = str(params['ac'])
   dr = str(params['dr'])
   lr = str(params['lr'])
-  hn = str(params['hn'])
   hf = str(params['hf'])
   #subprocess.check_call(["./train.py", "-d", "ml_100k","--accum",  str(params['ac']),"-do",str(params['dr']),"-nleft", "-nb" , "2", "-e",str(params['epoch']),"--features", "--feat_hidden", str(params['hf']),"--testing" ])
-  subprocess.call("python train.py -d ml_100k --accum " + ac +" -do " + dr + " -nleft -nb 2 -e " + epoch + " --features --feat_hidden "+ hf +" --testing --hidden " + hn, shell=True)
+  subprocess.call("python train.py -d ml_100k --accum " + ac +" -do " + dr + " -nleft -nb 2 -e " + epoch + " --features --feat_hidden "+ hf +" --testing", shell=True)
 

1680ec1a57316277fe08ea8e90ecbd308c41fb6a
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-04 20:36:36 +1100

    Update grid.py

--- gcmc/grid.py
+++ gcmc/grid.py
@@ -1,4 +1,3 @@
 import subprocess
-from sklearn.grid_search import ParameterGrid
 
 subprocess.call("python train.py -d ml_100k --accum stack -do 0.7 -nleft -nb 2 -e 1000 --features --feat_hidden 10 --testing", shell=True)

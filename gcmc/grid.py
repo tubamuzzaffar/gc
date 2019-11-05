dd95ab0299cf30195b566989d37761c1f43a3dc1
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-05 20:05:36 +1100

    Update grid.py

--- gcmc/grid.py
+++ gcmc/grid.py
@@ -1,5 +1,8 @@
 import subprocess
 from sklearn.model_selection import ParameterGrid
+import random
+
+random.seed(50)
 
 param_grid = {'epoch' :[1000, 100, 50,500], 'lr':[0.0005, 0.001,0.00146,0.01,0.015,0.0175,0.02], 'dr':[0.03,0.4,0.5,0.6,0.7,0.8],'hf':[5,10,15], 'ac':['stack','sum'], 'hn':[(50,8),(10,7),(50,3),(100,25),(40,15),(500,75),(45,15)]}
 
@@ -7,12 +10,12 @@ grid = list(ParameterGrid(param_grid))
 
 for params in grid:
   print(params['ac'], str(params['dr']), str(params['lr']), str(params['hf']))
-  epoch = str(params['epoch'])
-  ac = str(params['ac'])
-  dr = str(params['dr'])
-  lr = str(params['lr'])
-  hf = str(params['hf'])
-  x = params['hn']
+  epoch = str(random.sample(params['epoch'],1)[0])
+  ac = str(random.sample(params['ac'],1)[0])
+  dr = str(random.sample(params['dr'],1)[0])
+  lr = str(random.sample(params['lr'],1)[0])
+  hf = str(random.sample(params['hf'],1)[0])
+  x = random.sample(params['hn'],1)[0]
   first = str(x[0])
   second = str(x[1])
 

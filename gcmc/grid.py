00162244834d747c46b9be4b33f95d1c5bfd1034
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-05 20:22:12 +1100

    Update grid.py

--- gcmc/grid.py
+++ gcmc/grid.py
@@ -8,9 +8,12 @@ param_grid = {'epoch' :[1000, 100, 50,500], 'lr':[0.0005, 0.001,0.00146,0.01,0.0
 
 grid = list(ParameterGrid(param_grid))
 
+e_val = random.sample(grid['epoch'],1)[0]
+print(e_val)
+
 for params in grid:
   print(params['ac'], str(params['dr']), str(params['lr']), str(params['hf']))
-  print(len(params['epoch']))
+  epoch = str(params['epoch'])
   #e_val = random.sample(params['epoch'],1)[0]
   ac = str(params['ac'])
   dr = str(params['dr'])

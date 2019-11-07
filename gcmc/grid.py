652c82afb3fc116c030b5f084435a69a7d9ac535
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-08 07:58:58 +1100

    Update grid.py

--- gcmc/grid.py
+++ gcmc/grid.py
@@ -4,11 +4,11 @@ import random
 
 random.seed(50)
 
-param_grid = {'epoch' :[1000, 100, 50,500], 'lr':[0.0005, 0.001,0.00146,0.01,0.015,0.0175,0.02], 'dr':[0.03,0.4,0.5,0.6,0.7,0.8],'hf':[5,10,15], 'ac':['stack','sum'], 'hn':[(50,8),(10,7),(50,3),(100,25),(40,15),(500,75),(45,15)]}
+param_grid = {'epoch' :[1000, 100, 50,500], 'lr':[0.0005, 0.001,0.00146,0.01,0.015,0.0175,0.02], 'dr':[0.03,0.4,0.5,0.6,0.7,0.8],'hf':[5,10,15], 'ac':['stack','sum'], 'hn':[(50,8),(10,7),(50,3),(100,25),(40,15),(500,75)]}
 #param_grid = {'hf':[5,10,15]}
 grid = list(ParameterGrid(param_grid))
 
-e_val = random.sample(grid, 100)
+e_val = random.sample(grid, 1000)
 #print(e_val)
 
 for params in e_val:

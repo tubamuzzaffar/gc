976407bf29215ac01d239003f47f1e9ebd90532a
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-10 20:05:23 +1100

    Update hn.py

--- gcmc/hn.py
+++ gcmc/hn.py
@@ -18,4 +18,4 @@ for params in grid:
   #subprocess.check_call(["./train.py", "-d", "ml_100k","--accum",  str(params['ac']),"-do",str(params['dr']),"-nleft", "-nb" , "2", "-e",str(params['epoch']),"--features", "--feat_hidden", str(params['hf']),"--testing" ])
   #subprocess.call("python train.py -d ml_100k --accum " + ac +" -do " + dr + " -nleft -nb 2 -e " + epoch + " --features --feat_hidden "+ hf +" --testing --learning_rate " + lr + " --hidden " + first +" "+ second ,shell=True)
   #subprocess.call("python train.py -d ml_100k --accum stack -do 0.7 -nleft -nb 2 -e 1000 --features --feat_hidden "+ hf +" --testing",shell=True)
-  subprocess.call("python train.py -d ml_100k --accum stack -do "+ dr +" -nleft -nb 2 -e 1000 --features --feat_hidden 10 --testing --hidden " + first + second)
+  subprocess.call("python train.py -d ml_100k --accum stack -do 0.7 -nleft -nb 2 -e 1000 --features --feat_hidden 10 --testing --hidden " + first + " "+ second)

4a263d08df4fa094631d17862da289609302fd50
Tuba Muzzaffar <tmuz0631@uni.sydney.edu.au>
2019-11-10 20:06:38 +1100

    Update lr.py

--- gcmc/lr.py
+++ gcmc/lr.py
@@ -1,4 +1,4 @@
-mport subprocess
+import subprocess
 from sklearn.model_selection import ParameterGrid
 import random
 
@@ -13,7 +13,7 @@ for params in grid:
   lr = str(params['lr'])
 
   #subprocess.check_call(["./train.py", "-d", "ml_100k","--accum",  str(params['ac']),"-do",str(params['dr']),"-nleft", "-nb" , "2", "-e",str(params['epoch']),"--features", "--feat_hidden", str(params['hf']),"--testing" ])
-  subprocess.call("python train.py -d ml_100k --accum stack -do 0.7 -nleft -nb 2 -e 1000 --features --feat_hidden 10 --testing --learning_rate" + lr)
+  subprocess.call("python train.py -d ml_100k --accum stack -do 0.7 -nleft -nb 2 -e 1000 --features --feat_hidden 10 --testing --learning_rate " + lr, shell=True)
 
   #subprocess.call("python train.py -d ml_100k --accum " + ac +" -do " + dr + " -nleft -nb 2 -e " + epoch + " --features --feat_hidden "+ hf +" --testing --learning_rate " + lr + " --hidden " + first +" "+ second ,shell=True)
   #subprocess.call("python train.py -d ml_100k --accum stack -do 0.7 -nleft -nb 2 -e 1000 --features --feat_hidden "+ hf +" --testing",shell=True)
